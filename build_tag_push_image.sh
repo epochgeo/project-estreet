8c1a886475c1c34b45a01e859bc3479c7dfa81c1
Isaac <isaac.michael.craig@hotmail.com>
2025-06-06 08:17:30 -0400

    added login for docker

--- build_tag_push_image.sh
+++ build_tag_push_image.sh
@@ -1,6 +1,8 @@
 export aws_acc_id=$(aws sts get-caller-identity --query Account --output text)
 echo using aws acc id: $aws_acc_id
 
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $aws_acc_id.dkr.ecr.us-east-1.amazonaws.com
+
 docker compose up --build --force-recreate -d && docker compose down
 docker tag project-estreet:latest $aws_acc_id.dkr.ecr.us-east-1.amazonaws.com/project-estreet
 #aws ecr list-images --repository-name=project-estreet --region=us-east-1
